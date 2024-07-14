8cd52d14599b5633e78204b09b185d30fddcf306
Wojciech <wojciechwroobel@gmail.com>
2024-07-14 16:49:39 +0200

    Created function dividing, adding, multiplying or subtracting two numbers - exercise 14

--- main.js
+++ main.js
@@ -143,3 +143,19 @@ function removeFirstAndLastCharacter(str){
 }
 
 console.log(removeFirstAndLastCharacter("Amazing"));
+
+// 14
+
+function getBasicOperationsResult(operation, value1, value2){
+    if (operation == '+') {
+        return value1 + value2;
+    } else if (operation == '-') {
+        return value1 - value2;
+    } else if (operation == '*') {
+        return value1 * value2;
+    }else if (operation == '/') {
+        return value1 / value2;
+    }
+}
+
+console.log(getBasicOperationsResult("+", 5, 10));
